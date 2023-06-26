4f47dccbe5b4eba76e3918de46c4ad7cf5e6bdf4
kimwells00 <wellskimberly22@gmail.com>
2023-06-26 14:54:21 -0400

    added comments

--- e-commerce-react-app/src/components/Cart/Cart.jsx
+++ e-commerce-react-app/src/components/Cart/Cart.jsx
@@ -1,16 +1,20 @@
 /* eslint-disable react/prop-types */
 export default function Cart({ cart }) {
   //calculate cart total
+  //.reduce() method to iterate over each item in the cart array and accumulate the total price.
   const totalPrice = cart.reduce(
     (total, item) => total + item.price * item.quantity,
     0
   );
   return (
     <div>
+      {/* renders cart info */}
       <h2>Cart</h2>
+
       {cart.map((item, index) => (
-        <div key={index}>
+        <div key={index} className="cart-card">
           <p>{item.name}</p>
+          <img src={item.image} alt={item.name} />
           <p>Price: ${item.price}</p>
           <p>Quantity: {item.quantity}</p>
         </div>
