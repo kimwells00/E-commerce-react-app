583acb19204a4b397b9f9457954d61117a56ece0
kimwells00 <wellskimberly22@gmail.com>
2023-06-26 14:53:42 -0400

    added comments to handlecart function

--- e-commerce-react-app/src/App.jsx
+++ e-commerce-react-app/src/App.jsx
@@ -11,23 +11,23 @@ import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 function App() {
   const plants = [
     {
-      name: "Albino Monstera",
+      name: "Monstera",
       scientificName: "	Monstera deliciosa 'Albo-Variegata'",
       image:
-        "https://images.unsplash.com/photo-1560415578-57d16c5df143?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=388&q=80",
+        "https://bloomscape.com/cdn-cgi/image/quality=75,fit=scale-down,height=389,width=324,metadata=copyright,format=webp/wp-content/uploads/2020/08/bloomscape_monstera_stone-e1633461206253.jpg",
       description:
-        "The Albino Monstera has large, heart-shaped leaves with irregular patches of white or creamy-yellow coloration. The variegation can occur in different patterns, such as spots, streaks, or marbling. The variegated areas are devoid of chlorophyll, which gives them their distinctive color.",
+        "The Monstera is characterized by its large, glossy, heart-shaped leaves that have distinct, natural leaf holes called fenestrations. The leaves can grow to impressive sizes, often reaching up to 2 feet (60 cm) in length. The plant has a climbing habit, and as it matures, it develops aerial roots that can latch onto surfaces for support.",
       lighting:
-        "The Albino Monstera prefers bright, indirect light. Avoid placing it in direct sunlight, as it can scorch the delicate variegated leaves. A well-lit spot near a window, where it receives filtered or dappled sunlight, is ideal.",
+        "Monstera plants prefer bright, indirect light. They can tolerate some direct sunlight, but it's best to avoid exposing them to intense, harsh sunlight, as it can scorch their leaves. Place them near a window with filtered light or provide them with bright artificial lighting if grown indoors.",
       watering:
-        "It's important to maintain a proper watering routine for the Albino Monstera. Allow the top inch or so of the soil to dry out between waterings, and then water thoroughly until water drains from the bottom of the pot. Avoid overwatering, as it can lead to root rot.",
-      price: 100.0,
+        "Monstera plants have moderate watering needs. They prefer a well-draining soil mix that retains some moisture but doesn't become waterlogged. Allow the top inch (2.5 cm) of soil to dry between waterings, and then water thoroughly, ensuring excess water drains out. Avoid overwatering, as it can lead to root rot.",
+      price: 170.0,
     },
     {
       name: "Fiddle leaf fig tree",
       scientificName: "Ficus lyrata",
       image:
-        "https://images.unsplash.com/photo-1545239705-1564e58b9e4a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80",
+        "https://bloomscape.com/cdn-cgi/image/quality=75,fit=scale-down,height=390,width=324,metadata=copyright,format=webp/wp-content/uploads/2020/08/bloomscape_fiddle-leaf-fig_charcoal-e1652800894846.jpg",
       description:
         "The Fiddle-Leaf Fig tree has large, violin-shaped leaves that are dark green and leathery in texture. The leaves are deeply veined and have a glossy sheen, adding to their aesthetic appeal. The tree can grow up to several feet tall, creating an impressive focal point in any indoor space.",
       lighting:
@@ -37,23 +37,23 @@ function App() {
       price: 60.0,
     },
     {
-      name: "Donkey's tail",
-      scientificName: "Sedum morganianum",
+      name: "Triostar",
+      scientificName: "stromanthe triostar",
       image:
-        "https://assets.weimgs.com/weimgs/rk/images/wcm/products/202311/0131/faux-potted-donkey-tail-plant-o.jpg",
+        "https://bloomscape.com/cdn-cgi/image/quality=75,fit=scale-down,height=921,width=768,metadata=copyright,format=webp/wp-content/uploads/2022/05/bloomscape_stromanthe_triostar_large_stone-scaled.jpg",
       description:
-        " The Donkey's Tail plant features long, trailing stems covered in closely packed, fleshy leaves. The leaves are cylindrical and resemble small, plump beads. They have a bluish-green color and a smooth, glossy texture. The stems can grow several feet long, creating a cascading effect when the plant is displayed in hanging baskets or elevated positions.",
+        " Stromanthe Triostar features large, lance-shaped leaves with striking variegation. The leaves have a mix of colors including shades of green, cream, and pink. The variegation creates a beautiful and eye-catching pattern, adding a touch of tropical elegance to any space.",
       lighting:
-        "The Donkey's Tail plant thrives in bright, indirect light. It appreciates ample sunlight but should be protected from intense, direct sunlight, as it can scorch the leaves. Place it near a window where it can receive bright, filtered light for several hours a day.",
+        "Stromanthe Triostar prefers bright, indirect light. It thrives in locations with filtered sunlight or in areas where it receives bright but indirect light. Avoid exposing it to direct sunlight, as it can scorch the leaves. Inadequate light may result in reduced variegation and slower growth.",
       watering:
-        "As a succulent, the Donkey's Tail has low water requirements. Allow the soil to dry out completely between waterings and then water the plant thoroughly. Overwatering can lead to root rot, so it's important to avoid waterlogged conditions. Water sparingly during the winter months when the plant's growth slows down.",
+        "Stromanthe Triostar prefers slightly moist soil. Water the plant when the top inch of the soil feels dry, ensuring thorough watering without allowing it to sit in standing water. It is important to maintain adequate humidity levels around the plant by misting the leaves or placing a tray of water nearby.",
       price: 40.0,
     },
     {
       name: "Elephant's ear",
       scientificName: "Colocasia",
       image:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQUVF1pIi-yJOheyIESPmK6EfUGfbrat_yh_Q&usqp=CAU",
+        "https://i.pinimg.com/736x/ba/5d/93/ba5d935842bb465025a66862980d4936.jpg",
       description:
         "The Elephant Ear plant is characterized by its large, heart-shaped or arrowhead-shaped leaves that grow on long stalks. The leaves can vary in size, depending on the species and cultivar, but they are typically broad and impressive, giving the plant its unique and recognizable look. The foliage comes in various shades of green, and some varieties may have interesting patterns or variegation.",
       lighting:
@@ -63,8 +63,8 @@ function App() {
       price: 50.0,
     },
     {
-      name: "Pothos",
-      scientificName: "Devil's ivy",
+      name: "Devil's ivy",
+      scientificName: "Pothos",
       image:
         "https://images.unsplash.com/photo-1537039557108-4a42c334fd5e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=435&q=80",
       description:
@@ -90,9 +90,15 @@ function App() {
     },
   ];
   const [cart, setCart] = useState([]);
+  // takes a plant object as a parameter
   const handleAddToCart = (plant) => {
+    //This line checks if the plant already exists in the cart array
+    //find() method to search for an item in the cart array that has the same name as the plant being added.
     const plantInCart = cart.find((item) => item.name === plant.name);
+    //checks if the plant is already present in the cart
     if (plantInCart) {
+      //creates an updated version of the cart array by mapping over each item in the cart array
+      // and increasing the quantity of the matching plant by 1.
       const updatedCart = cart.map((item) => {
         if (item.name === plant.name) {
           return { ...item, quantity: item.quantity + 1 };
@@ -100,7 +106,10 @@ function App() {
         return item;
       });
       setCart(updatedCart);
+      //If the plant is not in the cart this else block is executed
     } else {
+      //adds a new item to the cart array by spreading the  cart array and
+      // adding a new  plant with a quantity of 1 ({ ...plant, quantity: 1 }).
       setCart([...cart, { ...plant, quantity: 1 }]);
     }
   };
