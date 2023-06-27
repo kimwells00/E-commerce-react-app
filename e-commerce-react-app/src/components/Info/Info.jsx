def4298a8567211a32bb85236508ad7d268efcd5
kimwells00 <wellskimberly22@gmail.com>
2023-06-26 21:58:11 -0400

    updated info page

--- e-commerce-react-app/src/components/Info/Info.jsx
+++ e-commerce-react-app/src/components/Info/Info.jsx
@@ -1,7 +1,24 @@
+/* eslint-disable react/no-unescaped-entities */
 export default function Info() {
   return (
-    <div>
-      <h1>Info</h1>
+    <div className="info-page">
+      <p className="info-card">
+        {" "}
+        Welcome to our plant paradise! We are passionate about bringing the
+        beauty and benefits of plants right to your doorstep. <br /> At
+        Botanical Haven, we offer a wide variety of exquisite plants that will
+        transform your space into a lush and vibrant oasis. <br /> Whether
+        you're a seasoned plant enthusiast or just starting your green journey,
+        our carefully curated collection caters to all levels of plant lovers.{" "}
+        <br /> We believe in the power of nature to enhance our lives and create
+        a sense of tranquility and well-being. <br /> With our commitment to
+        quality, exceptional customer service, and expert advice, we strive to
+        make your plant shopping experience delightful and rewarding. <br />{" "}
+        Explore our selection, find the perfect green companion, and let
+        nature's wonders breathe life into your home or office. <br /> Join us
+        in celebrating the beauty of plants and discover the joy of nurturing
+        your very own botanical haven.
+      </p>
     </div>
   );
 }
