4ddae8b24a2cfdd84ea5ce593068353c90d79519
kimwells00 <wellskimberly22@gmail.com>
2023-06-26 21:48:23 -0400

    added keep shopping button

--- e-commerce-react-app/src/components/PlantDetails/PlantDetails.jsx
+++ e-commerce-react-app/src/components/PlantDetails/PlantDetails.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable react/prop-types */
-
+import { Link } from "react-router-dom";
 import { useParams, useNavigate } from "react-router-dom";
 
 export default function PlantDetails({ plants, handleAddToCart }) {
@@ -14,17 +14,23 @@ export default function PlantDetails({ plants, handleAddToCart }) {
 
   return (
     <div className="plant-details-container">
-      <h1>{plant.name}</h1>
-      <img src={plant.image} alt={plant.name} />
-
-      <p>{plant.description}</p>
-      <h2>CARE INSTRUCTIONS</h2>
-      <h3>Lighting</h3>
-      <p>{plant.lighting}</p>
-      <h3>Watering</h3>
-      <p>{plant.watering}</p>
-      <p>Price: ${plant.price}</p>
-      <button onClick={addToCart}>Add to cart</button>
+      <div className="plant-image">
+        <img src={plant.detailImage} alt={plant.name} />
+      </div>
+      <div className="plant-info">
+        <h1>{plant.name}</h1>
+        <p>{plant.description}</p>
+        <h2>CARE INSTRUCTIONS</h2>
+        <h3>Lighting</h3>
+        <p>{plant.lighting}</p>
+        <h3>Watering</h3>
+        <p>{plant.watering}</p>
+        <p>Price: ${plant.price}</p>
+        <button onClick={addToCart}>Add to cart</button>
+        <button className="keep-shopping-button">
+          <Link to={`/products`}> Keep Shopping </Link>
+        </button>
+      </div>
     </div>
   );
 }
