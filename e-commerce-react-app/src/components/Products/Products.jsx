4d0473acf9dd8aaf3e8fcc5eabf5a98b19ff44fd
kimwells00 <wellskimberly22@gmail.com>
2023-06-26 21:46:28 -0400

    added header

--- e-commerce-react-app/src/components/Products/Products.jsx
+++ e-commerce-react-app/src/components/Products/Products.jsx
@@ -4,13 +4,16 @@ import { Link } from "react-router-dom";
 export default function Products({ plants }) {
   return (
     <div className="product-card-container">
+      <div className="shop-header">
+        <h1>Shop Our Plants</h1>
+      </div>
       {plants.map((plant) => (
         <div className="product-card" key={plant.name}>
           <h2>{plant.name}</h2>
           <h3>{plant.scientificName}</h3>
           <img src={plant.image} alt={plant.name} />
 
-          <button>
+          <button className="more-info-button">
             <Link to={`/products/${plant.name}`}> more info </Link>
           </button>
         </div>
